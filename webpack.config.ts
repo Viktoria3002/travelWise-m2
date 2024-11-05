3316efcf125069222a3d5fa7355228e7aaadd4a5
Viktoria Dyakova <viktoria.guliants@yandex.ru>
2024-11-05 20:55:45 +0300

    webpack-edit | add: typeScript and loaders

---
+++ webpack.config.ts
@@ -0,0 +1,57 @@
+/* eslint-disable import/no-anonymous-default-export */
+import path from 'path';
+import webpack from 'webpack';
+import HtmlWebpackPlugin from 'html-webpack-plugin';
+
+type Mode = 'production' | 'development';
+
+interface EnvVariables {
+  mode: Mode;
+}
+
+export default (env: EnvVariables) => {
+  const config: webpack.Configuration = {
+    mode: env.mode ?? 'development',
+    entry: path.resolve(__dirname, 'src', 'index.ts'),
+    output: {
+      path: path.resolve(__dirname, 'build'),
+      filename: '[name].[contenthash].js',
+      clean: true,
+    },
+    plugins: [
+      new HtmlWebpackPlugin({ template: path.resolve(__dirname, 'public', 'index.html') }),
+      new webpack.ProgressPlugin(),
+    ],
+    module: {
+      rules: [
+        {
+          test: /\tsx?$/,
+          use: 'ts-loader',
+          exclude: /node_modules/,
+        },
+      ],
+    },
+    resolve: {
+      extensions: ['tsx', '.ts', 'js'],
+    },
+
+    // module: {
+    //   rules: [
+    //     {
+    //       test: /\.jsx?$/,
+    //       exclude: /node_modules/,
+    //       use: {
+    //         loader: 'babel-loader',
+    //         options: {
+    //           presets: ['@babel/preset-env', '@babel/preset-react'],
+    //         },
+    //       },
+    //     },
+    //   ],
+    // },
+    // resolve: {
+    //   extensions: ['.js', '.jsx'],
+    // },
+  };
+  return config;
+};
