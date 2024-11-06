b43c0a0134aadbefa171ad4779d06596f0155062
Viktoria Dyakova <viktoria.guliants@yandex.ru>
2024-11-06 07:53:26 +0300

    webpack-edit | add: MiniCssExtractPlugin and css, scss

---
+++ src/components/App.tsx
@@ -0,0 +1,20 @@
+import { useState } from 'react';
+import './App.scss';
+
+export const App = () => {
+  const [count, setCount] = useState(0);
+
+  const increment = () => {
+    setCount((prev) => prev + 1);
+  };
+  return (
+    <div>
+      <h1>{count}</h1>
+      <button onClick={increment}>
+        <span>Inc</span>
+      </button>
+    </div>
+  );
+};
+
+export default App;
--- webpack.config.ts
+++ webpack.config.ts
@@ -3,6 +3,7 @@ import path from 'path';
 import webpack from 'webpack';
 import HtmlWebpackPlugin from 'html-webpack-plugin';
 import type { Configuration as DevServerConfiguration } from 'webpack-dev-server';
+import MiniCssExtractPlugin from 'mini-css-extract-plugin';
 
 type Mode = 'production' | 'development';
 
@@ -13,10 +14,11 @@ interface EnvVariables {
 
 export default (env: EnvVariables) => {
   const isDev = env.mode === 'development';
+  const isProd = env.mode === 'production';
 
   const config: webpack.Configuration = {
     mode: env.mode ?? 'development',
-    entry: path.resolve(__dirname, 'src', 'index.ts'),
+    entry: path.resolve(__dirname, 'src', 'index.tsx'),
     output: {
       path: path.resolve(__dirname, 'build'),
       filename: '[name].[contenthash].js',
@@ -25,9 +27,18 @@ export default (env: EnvVariables) => {
     plugins: [
       new HtmlWebpackPlugin({ template: path.resolve(__dirname, 'public', 'index.html') }),
       isDev && new webpack.ProgressPlugin(),
+      isProd &&
+        new MiniCssExtractPlugin({
+          filename: 'css/[name].[contenthash:8].css',
+          chunkFilename: 'css/[name].[contenthash:8].css',
+        }),
     ].filter(Boolean),
     module: {
       rules: [
+        {
+          test: /\.s[ac]ss$/i,
+          use: [isDev ? 'style-loader' : MiniCssExtractPlugin.loader, 'css-loader', 'sass-loader'],
+        },
         {
           test: /\.tsx?$/,
           use: 'ts-loader',
