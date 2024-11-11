3b8a0d33cab163075549143374741bc4ea70b4dc
Viktoria Dyakova <viktoria.guliants@yandex.ru>
2024-11-12 00:31:33 +0300

    webpack-edit | add: assets loaders

--- config/build/buildLoaders.ts
+++ config/build/buildLoaders.ts
@@ -4,6 +4,34 @@ import { BuildOptions } from './types/types';
 
 export function buildLoaders(options: BuildOptions): ModuleOptions['rules'] {
   const isDev = options.mode === 'development';
+
+  const assetLoader = {
+    test: /\.(png|jpg|jpeg|gif)$/i,
+    type: 'asset/resource',
+  };
+
+  const svgrLoader = {
+    test: /\.svg$/,
+    use: [
+      {
+        loader: '@svgr/webpack',
+        options: {
+          icon: true,
+          svgoConfig: {
+            plugins: [
+              {
+                name: 'convertColors',
+                params: {
+                  currentColor: true,
+                },
+              },
+            ],
+          },
+        },
+      },
+    ],
+  };
+
   const cssLoaderWithModules = {
     loader: 'css-loader',
     options: {
@@ -27,5 +55,5 @@ export function buildLoaders(options: BuildOptions): ModuleOptions['rules'] {
     use: 'ts-loader',
     exclude: /node_modules/,
   };
-  return [scssLoader, tsLoader];
+  return [assetLoader, scssLoader, tsLoader, svgrLoader];
 }
--- src/components/App/App/App.tsx
+++ src/components/App/App/App.tsx
@@ -2,15 +2,25 @@ import { useState } from 'react';
 import classes from './App.module.scss';
 import { Link, Outlet } from 'react-router-dom';
 import About from '@/pages/about/About';
+import platePng from '@/assets/plate.png';
+import plateJpg from '@/assets/plate.jpg';
+import Icon from '@/assets/icon.svg';
 
 export const App = () => {
-  const [count, setCount] = useState(0);
+  const [count, setCount] = useState<number>(0);
 
   const increment = () => {
     setCount((prev) => prev + 1);
   };
   return (
     <div>
+      <div>
+        <img width={100} height={100} src={platePng} alt="" />
+        <img width={100} height={100} src={plateJpg} alt="" />
+      </div>
+      <div>
+        <Icon className={classes.icon} width={50} height={50} fill={'red'} />
+      </div>
       <Link to={'/about'}>about</Link>
       <br />
       <Link to={'/shop'}>shop</Link>
